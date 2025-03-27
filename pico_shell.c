573904865a4aafafcf880ba0487528a214c18045
Nourishere <nournawar5@gmail.com>
2025-03-27 18:56:24 +0200

    Added the built-in functions in the parent process

--- pico_shell.c
+++ pico_shell.c
@@ -27,55 +27,53 @@ int main(int argc, char* argv[]){
 			fgets(buff,MAX_BUF,stdin);
 			buff[strlen(buff)-1] = 0;
 			if(strlen(buff) == 0){
-				continue;	
+				continue;
 			}
 			char ** newarg = parse_input(buff);
-			pid_t pid = fork();	
-			if(pid > 0){
-				/* we are in the parent process */
-				int status;
-				while (wait(&status)>1){;
-					if(WEXITSTATUS(status) == EXIT_STATUS){
-						exit(0);
-					}
-				}		
-			}
-			else if(pid==0){
-				/* we are in the child process */
-
-				if(0 == strcmp(newarg[0],"exit")){
+			if(0 == strcmp(newarg[0],"exit")){
 					exit(EXIT_STATUS);
-				}
-				else if(0 == strcmp(newarg[0],"pwd")){
+			}
+			else if(0 == strcmp(newarg[0],"pwd")){
 					my_pwd();
-					free_looped(newarg);
-					exit(0);
+			}
+			else if(0 == strcmp(newarg[0],"cd")){
+					my_cd(newarg);
+			}
+			else if(0 == strcmp(newarg[0],"echo")){
+					my_echo(newarg);
+			}
+			/* If the command is not a built it, fork */
+			else{
+				pid_t pid = fork();
+
+				if(pid > 0){
+					/* we are in the parent process */
+					int status;
+					if(0 == strcmp(newarg[0],"cd")){
+							my_cd(newarg);
+					}
+					while (wait(&status)>1){;
+						if(WEXITSTATUS(status) == EXIT_STATUS){
+							exit(0);
+						}
+					}
 				}
-				else if(0 == strcmp(newarg[0],"cd")){
-						my_cd(newarg);
-						free_looped(newarg);
-						exit(0);
-				}
-				else if(0 == strcmp(newarg[0],"echo")){
-						my_echo(newarg);
-						free_looped(newarg);
-						exit(0);
-				}
-				else{
-					execvp(newarg[0],newarg);
-					printf("Error\n");
-					free_looped(newarg);
-					exit(-1);
+				else if(pid==0){
+					/* we are in the child process */
+
+						execvp(newarg[0],newarg);
+						printf("Error\n");
+						exit(-1);
+
 				}
 			}
-		}
-	}
-
+        }
+    }
 }
 
 
 char ** parse_input(char * input_buff){
-	char **args = malloc(sizeof(char *) * INIT_SIZE);		
+	char **args = malloc(sizeof(char *) * INIT_SIZE);
 	int k = 0, argcount = 0;
 	int size = INIT_SIZE; /* The array has an initial INIT_SIZE slots */
 	int start;
@@ -89,7 +87,7 @@ char ** parse_input(char * input_buff){
 		for (; input_buff[k] != '\0' && input_buff[k] != ' '; k++) {}
 		if ((k-start) > 0){
 			char *word = malloc((k - start) + 1); /* plus one for null termination */
-			word[k-start] = '\0';	
+			word[k-start] = '\0';
 			strncpy(word,&input_buff[start], k-start);
 			if(argcount >= size -1){
 				size*=2;
@@ -106,7 +104,6 @@ int i;
 	for(i=0;arr[i];i++)
 		free(arr[i]);
 	free(arr);
-
 }
 
 /* Built in functions */
@@ -115,7 +112,7 @@ void my_pwd(){
 	if(NULL == p){
 		write(2,"Error using mypwd\n",19);
 	}
-	else{	
+	else{
 		write(1,p,strlen(p));
 		write(1,"\n",1);
 	}
@@ -135,7 +132,7 @@ void my_echo(char** newarg){
 		printf("\n");
 }
 void my_cd(char** newarg){
-	err = chdir(newarg[1]);	
+	err = chdir(newarg[1]);
 	if (err == -1){
 		printf("Error: no such file or directory\n");
 		free_looped(newarg);
